c3f208fcd501fac0f8c8090e905f1c668bb5212f
Alone45-45 <85700450+Alone45-45@users.noreply.github.com>
2022-06-12 22:58:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,15 +6,15 @@ admins = {}
 load_dotenv()
 
 # client vars
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
-BOT_TOKEN = getenv("BOT_TOKEN")
-SESSION_NAME = getenv("SESSION_NAME", "session")
+API_ID = int(getenv("API_ID", "9683694"))
+API_HASH = getenv("API_HASH", "c426d9f7087744afdafc961a620b6338")
+BOT_TOKEN = getenv("BOT_TOKEN", "5551900409:AAENnaoRPQUHtLlPXkv-umxlfRBxfxD23P0")
+SESSION_NAME = getenv("SESSION_NAME", "AQA_Zr-dSuiQzy8JbTlvvDlu_ptag17xcaMFY5pvat--xFgoo04v_GvqKURxgTvYQRw3kUcGb03KERyQkEGWfSQ5HesbdJvlUbiqJnXL6bX_AwA8oPMhFBOYMGJOIdn4D-54pAna_xWqxp5bNanWh8zsz8IHugj5EB8974v_1tMDtp6KKCRqpaC_-br89hoY7fm9dWnxErDfCE7qfezs__3Yl_9rJwmsAMLiQNP9Z7C0OpCCHsFgakUKnWI736DmOXN7hm3bJY057jf79vHrRV1cK1xyhmjqkCTNwYZ7fbQRehktmdR0pI0E9gTseqlGOYOuGVb-8HT3ZdBKI9yYafu9AAAAAUpvbkwA")
 
 # mandatory vars
-OWNER_USERNAME = getenv("OWNER_USERNAME")
-ALIVE_NAME = getenv("ALIVE_NAME")
-BOT_USERNAME = getenv("BOT_USERNAME")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "TASTRON")
+ALIVE_NAME = getenv("ALIVE_NAME", "Tom")
+BOT_USERNAME = getenv("BOT_USERNAME", "WMTB_BOT")
 UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Alone45-45/Nobita")
 UPSTREAM_BRANCH = getenv("UPSTREM_BRANCH", "main")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "60"))
@@ -22,10 +22,10 @@ GROUP_SUPPORT = getenv("GROUP_SUPPORT", "The_Xmenteam")
 UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "TheJerryNetwork")
 
 # database, decorators, handlers mandatory vars
-MONGODB_URL = getenv("MONGODB_URL")
+MONGODB_URL = getenv("MONGODB_URL", "mongodb+srv://noobyash:noobyash@cluster0.0mptw.mongodb.net/?retryWrites=true&w=majority")
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! . $").split())
-OWNER_ID = list(map(int, getenv("OWNER_ID").split()))
-SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
+OWNER_ID = list(map(int, getenv("OWNER_ID", "5286943475").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5286943475").split()))
 
 # image resources vars
 IMG_1 = getenv("IMG_1", "https://telegra.ph/file/d6f92c979ad96b2031cba.png")
