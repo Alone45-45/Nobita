26237bf515000b0e510d6de5a72aae932f09f51e
Alone45-45 <85700450+Alone45-45@users.noreply.github.com>
2022-06-23 15:12:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
-import asyncio
+import requests
 from pytgcalls import idle
-from driver.core import calls, bot, user
+from callsmusic import run
+from handlers import __version__
+from pyrogram import Client as Bot
+from config import API_HASH, API_ID, BG_IMAGE, BOT_TOKEN
 
 
-async def start_bot():
-    await bot.start()
-    print("[INFO]: BOT & UBOT CLIENT STARTED !!")
-    await calls.start()
-    print("[INFO]: PY-TGCALLS CLIENT STARTED !!")
-    await user.join_chat("chatty_familia")
-    await user.join_chat("Some_Vibes")
-    await idle()
-    print("[INFO]: STOPPING BOT & USERBOT")
-    await bot.stop()
+response = requests.get(BG_IMAGE)
+with open("./etc/foreground.png", "wb") as file:
+    file.write(response.content)
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(start_bot())
+
+bot = Bot(
+    ":memory:",
+    API_ID,
+    API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="handlers"),
+)
+
+print(f"[INFO]: VEEZ MUSIC v{__version__} STARTED !")
+
+bot.start()
+run()
+idle()
